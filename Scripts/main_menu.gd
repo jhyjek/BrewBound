4fef5469163394496664409833e554945a6ad160
Athish <Athish1123@gmail.com>
2025-01-17 23:53:13 -0600

    Added Volume button and slider

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -16,7 +16,7 @@ func _on_start_pressed() -> void:
 
 
 func _on_setting_pressed() -> void:
-	print("Setting Pressed")
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
 
 func _on_exit_pressed() -> void:
