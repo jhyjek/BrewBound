069a485681cf9264744b2d011057df1f8fd94b28
Athish <Athish1123@gmail.com>
2025-01-18 12:19:38 -0600

    Added code to make inventory know where and what item is which slot

--- Inventory/inventory_ui.gd
+++ Inventory/inventory_ui.gd
@@ -1,8 +1,14 @@
 extends Control
 
+@onready var inv: Inv = preload("res://Inventory/Player_Inventory.tres")
+@onready var slots: Array = $GridContainer.get_children()
 
+func update_slots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
+	
 func _ready() -> void:
-	pass
+	update_slots()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
