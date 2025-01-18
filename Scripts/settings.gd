84324b80ee2492dd31276e93263a23b0188668e6
Athish <Athish1123@gmail.com>
2025-01-18 00:05:41 -0600

    Added Volume value next to slider

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@onready var volume_label = $Label
+@onready var volume_slider = $MarginContainer/VBoxContainer/Volume
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	volume_slider.value_changed.connect(_on_volume_value_changed)
+	_on_volume_value_changed(volume_slider.value)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +18,7 @@ func _process(delta: float) -> void:
 func _on_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0,value)
 	if value == -30: AudioServer.set_bus_volume_db(0,-1000)
+	volume_label.text = str(value)
 		
 	
 
