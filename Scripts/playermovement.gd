dda76b92b2fcd432d4c0cb59f8e1d226bc512dac
Jacob <hyjek456@gmail.com>
2025-01-18 20:13:52 -0600

    movement fixed with animations working

--- Scripts/playermovement.gd
+++ Scripts/playermovement.gd
@@ -1,61 +1,43 @@
 extends CharacterBody2D
 
-@onready var player_sprite = $Sprite2D
-const SPEED = 100.0
-const JUMP_VELOCITY = -275.0
 @onready var animation = $AnimationPlayer
-@onready var crow = preload("res://Visual media/crowidle.png")
-@onready var wiz = preload("res://Visual media/wizidle.png")
+@onready var player_sprite = $Sprite2D
 
-const Gravity : int = 1000
-const  max_vel : int = 600
-const flap_speed : int = -500
-var crowmode :bool = false
+const SPEED = 110.0
+const JUMP_VELOCITY = -275.0
 
 
-
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("E"):
-		player_sprite.texture = crow
-	
-		
-	
-	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	if velocity.x < 0:
 		player_sprite.flip_h = true
 	elif  velocity.x > 0:
 		player_sprite.flip_h = false
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		animation.play("fall")
-		print ("falling")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("space") or Input.is_action_just_pressed("W") and is_on_floor():
+	if (Input.is_action_just_pressed("space") or Input.is_action_just_pressed("W")) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		animation.play("jump")
-		print("jumping")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("A", "D")
 	if direction:
-		if is_on_floor():
-			velocity.x = direction * SPEED
-			animation.play("walk")
-			print("walking")
+		velocity.x = direction * SPEED
 	else:
-		if is_on_floor():
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			animation.play("idle") 
-			print("idling")
-		
-		
-		
-		
-		
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	handle_animation()
+
+func handle_animation():
+	if is_on_floor():
+		if velocity:
+			animation.play("walk")
+		else:
+			animation.play("idle")
+	else:
+		animation.play("jump")
+		
+		
