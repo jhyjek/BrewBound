a5ae66273dce9f3176b10b2a51803c32918e0f87
Jacob <hyjek456@gmail.com>
2025-01-18 18:46:51 -0600

    jump brocken

--- Scripts/playermovement.gd
+++ Scripts/playermovement.gd
@@ -30,20 +30,31 @@ func _physics_process(delta: float) -> void:
 		player_sprite.flip_h = false
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		animation.play("fall")
+		print ("falling")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("space") and is_on_floor():
+	if Input.is_action_just_pressed("space") or Input.is_action_just_pressed("W") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animation.play("jump")
+		print("jumping")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("A", "D")
 	if direction:
-		velocity.x = direction * SPEED
-		animation.play("walk")
+		if is_on_floor():
+			velocity.x = direction * SPEED
+			animation.play("walk")
+			print("walking")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("idle") 
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			animation.play("idle") 
+			print("idling")
+		
+		
+		
 		
 		
 
