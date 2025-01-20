9b0fb90b7ebdfad37e4b5cf4bca2bacabb341940
Athish <Athish1123@gmail.com>
2025-01-20 14:18:19 -0600

    Made it so Items stack in the inventory

--- Inventory/Scripts/Inventory.gd
+++ Inventory/Scripts/Inventory.gd
@@ -4,11 +4,15 @@ class_name Inventory
 
 signal updated
 
-@export var items : Array[InventoryItem]
+@export var slots : Array[InventorySlot]
 
 func insert(item : InventoryItem):
-	for i in range(items.size()):
-		if !items[i]:
-			items[i] = item
-			break
+	var itemSlots = slots.filter(func(slot): return slot.item == item && slot.amount < slot.item.stackSize)
+	if !itemSlots.is_empty():
+		itemSlots[0].amount += 1
+	else:
+		var emptySlots = slots.filter(func(slot): return slot.item == null)
+		if !emptySlots.is_empty():
+			emptySlots[0].item = item
+			emptySlots[0].amount = 1
 	updated.emit() 
--- Inventory/Scripts/slot_ui.gd
+++ Inventory/Scripts/slot_ui.gd
@@ -2,10 +2,15 @@ extends Panel
 
 @onready var BackGroundSprite : Sprite2D = $BackGround
 @onready var ItemSprite : Sprite2D = $CenterContainer/Panel/Item
+@onready var amountLabel: Label = $CenterContainer/Panel/Label
 
-func update(item : InventoryItem):
-	if !item:
+func update(slot : InventorySlot):
+	if !slot.item:
 		ItemSprite.visible = false
+		amountLabel.visible = false
 	else:
 		ItemSprite.visible = true
-		ItemSprite.texture = item.texture
+		ItemSprite.texture = slot.item.texture
+		if slot.amount > 1:
+			amountLabel.visible = true
+			amountLabel.text = str(slot.amount)
